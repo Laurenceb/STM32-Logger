1ffab6b538c3b7f7095a9c7fc3ba303d13e5407a
Laurence Blaxter <Laurence.blaxter@btinternet.com>
2013-05-22 22:27:42 +0100

    Fixed timer simulator

--- tests/timsim/timsim.m
+++ tests/timsim/timsim.m
@@ -1,11 +1,20 @@
-function timers=timsim()
+function timers=timsim(updown)
 	numclks=6048*192*3;
 	timers=zeros(numclks,3);
 	timers(:,1)=repmat([1:6048]',192*3,1);
 	pwmout_one=zeros(numclks,1);
-	pwmout_one(find(timers(:,1)<5921))=1;
-	timers(:,2)=mod(cumsum(pwmout_one),5952);
-	pwmout_two=zeros(numclks,1);
-	pwmout_two(find(timers(:,1)<5984))=1;
-	timers(:,3)=mod(cumsum(pwmout_two),6048);
+	#if updown is true we go up then down, otherwise we go down then up
+	if updown
+		pwmout_one(find(timers(:,1)<=5983))=1;
+		timers(:,2)=mod(cumsum(pwmout_one),5952);
+		pwmout_two=zeros(numclks,1);
+		pwmout_two(find(timers(:,2)<=5920))=1;
+		timers(:,3)=mod(cumsum(pwmout_two),6048);		
+	else
+		pwmout_one(find(timers(:,1)<=5921))=1;
+		timers(:,2)=mod(cumsum(pwmout_one),5952);
+		pwmout_two=zeros(numclks,1);
+		pwmout_two(find(timers(:,2)<=5888))=1;
+		timers(:,3)=mod(cumsum(pwmout_two),5953);
+	endif
 endfunction
